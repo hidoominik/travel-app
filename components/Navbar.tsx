0e3af600c2992b4d6aa39fb883ca5ec6e1992d2a
hidoominik <dominik.choma.41@gmail.com>
2023-10-26 16:34:11 +0200

    navbar logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 max-container padding-container relative z-30 py-5">
-      <Link href="/"></Link>
+      <Link href="/">
+        <Image src="/hilink-logo.svg" width={74} alt="logo" height={29} />
+      </Link>
     </nav>
   );
 };
