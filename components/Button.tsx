8e52f3fd25401a597d79cb85b22cd2759f24470f
hidoominik <dominik.choma.41@gmail.com>
2023-10-27 18:01:27 +0200

    navbar

---
+++ components/Button.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import React from "react";
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      type={type}
+    >
+      {icon && <Image alt="icon" src={icon} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
