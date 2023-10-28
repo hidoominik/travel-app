1b88af390a1a1e9ab3afaa5f2b9ddfe5ee55414f
hidoominik <dominik.choma.41@gmail.com>
2023-10-28 10:08:59 +0200

    makes hamburger button works

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { NAV_LINKS } from "@/constants";
 import Button from "./Button";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsNavOpen(!isNavOpen);
+  };
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -27,14 +34,31 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
-
-      <Image
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
+      <div className="inline-block lg:hiden">
+        <Image
+          onClick={toggleMenu}
+          src="menu.svg"
+          alt="menu"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer lg:hidden"
+        />
+        {isNavOpen && (
+          <nav className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg py-2">
+            <ul className="h-full gap-12 lg:hidden">
+              {NAV_LINKS.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.key}
+                  className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        )}
+      </div>
     </nav>
   );
 };
