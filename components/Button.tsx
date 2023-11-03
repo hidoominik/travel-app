69061c7f4a12bdf4aaf3180ae6b922137795c60d
hidoominik <dominik.choma.41@gmail.com>
2023-11-03 19:56:29 +0100

    button improvements

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 type ButtonProps = {
@@ -5,12 +6,20 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const redirectToWebsite = () => {
+  // Replace the URL in the parentheses with the URL you want to redirect to
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+};
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
+      onClick={redirectToWebsite}
     >
       {icon && <Image alt="icon" src={icon} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
