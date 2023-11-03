fad20717d0643816ecf6a4089ba05b23657556ef
hidoominik <dominik.choma.41@gmail.com>
2023-11-03 19:56:46 +0100

    getapp section

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
-  return <div>GetApp</div>;
+  return (
+    <section className="flex-center w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get for free now!
+          </h2>
+          <p className="regular-16 text-gray-10">Avalible on Android and iOS</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Button
+              type="button"
+              title="App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+            <Button
+              type="button"
+              title="Google Play"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
+            />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" width={550} height={550} alt="phones" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetApp;
